a1661aa042da55c22f274198a58445b6fd7acc46
VictorNevesRevature <victor315@revature.net>
2022-11-29 18:46:46 -0500

    httpchanges

---
+++ HTTP/APITest.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HTTP
+{
+    public class APITest
+    {
+        public int id {get; set;}
+
+        
+    }
+}
+
+/*
+{"id":733,
+"name":"Kathy Ireland",
+"status":"Alive",
+"species":"Human",
+"type":"",
+"gender":"Female",
+"origin":
+{"name":"Earth (Replacement Dimension)",
+"url":"https://rickandmortyapi.com/api/location/20"},
+"location": {"name":"Earth (Replacement Dimension)",
+"url":"https://rickandmortyapi.com/api/location/20"},
+"image":"https://rickandmortyapi.com/api/character/avatar/733.jpeg",
+"episode":["https://rickandmortyapi.com/api/episode/45"],
+"url":"https://rickandmortyapi.com/api/character/733",
+"created":"2021-10-17T12:00:07.076Z"}
+*/
---
+++ HTTP/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HTTP;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpClient client = new HttpClient();
+
+        using (HttpResponseMessage resp = await client.GetAsync("https://rickandmortyapi.com/"))
+        {
+            string responseBody = await resp.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }
+        }
+        
+}
+
+
+/*
+{"id":733,"name":"Kathy Ireland","status":"Alive","species":"Human","type":"","gender":"Female","origin":
+{"name":"Earth (Replacement Dimension)","url":"https://rickandmortyapi.com/api/location/20"},"location":
+{"name":"Earth (Replacement Dimension)","url":"https://rickandmortyapi.com/api/location/20"},
+"image":"https://rickandmortyapi.com/api/character/avatar/733.jpeg","episode":["https://rickandmortyapi.com/api/episode/45"],
+"url":"https://rickandmortyapi.com/api/character/733","created":"2021-10-17T12:00:07.076Z"}
+*/
