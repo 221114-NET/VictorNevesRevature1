7f6f7b854c39843ef690492b908b35e1d75e3a38
VictorNevesRevature <victor315@revature.net>
2022-11-29 18:53:03 -0500

    idkman

---
+++ pokemonapp/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace pokemonapp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Object Instantion
+        Pokemon charmander = new Pokemon("charmander");
+
+        //You can use dot notation to access fields and methods availible to an object of that class
+        charmander.type = "fire";
+
+        // This is how you call instance method. It "belongs to" the class
+        charmander.isPokemon();
+
+        // This is how you call a class/static method. It "belongs to" the class
+        //Pokemon.Sound();
+    }
+}
+
+class Pokemon 
+{
+    //Fields
+    public string name;
+    public string type;
+    public int hitpoints;
+    public int dexNumber;
+    public int weight;
+    public int level;
+
+    //Constructor
+    public Pokemon(string name){
+        this.name = name;
+    }
+    public Pokemon(string pokename, string type, int hitpoints, int dexNumber, int weight, int level)
+    {
+        name = pokename;
+        this.type = type;
+        hitpoints = hp;
+        dexNumber = dex;
+        weight = wt;
+        level = lvl;
+
+
+
+    }
+    //Methods
+    public void isPokemon(){
+        Console.WriteLine($"My name is {name}. I am a {type} type. I am a pokemon.");
+    }
+}
+
+    //public static void Sound()
+    //{
+        //Console.WriteLine("*noises*");
+    //}
+
