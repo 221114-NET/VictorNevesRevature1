e404f37992054193bbfafe966b6ad46baba2b96a
VictorNevesRevature <victor315@revature.net>
2022-12-12 11:02:01 -0500

    removedserverlink

--- ApiProjectOneNew/RepoLayer/Class1.cs
+++ ApiProjectOneNew/RepoLayer/Class1.cs
@@ -1,11 +1,5 @@
 ﻿namespace RepoLayer;
 public class Class1
 {
-// Server=tcp:victor-p1server.database.windows.net,1433;
-//Initial Catalog=VictorProjectOne;
-//Persist Security Info=False;
-//User ID=victor203;
-//Password={your_password};
-//MultipleActiveResultSets=False;Encrypt=True;
-//TrustServerCertificate=False;Connection Timeout=30;
+
 }
