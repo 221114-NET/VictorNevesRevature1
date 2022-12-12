07e87e41bdf1bd1f2f4b23f55a2cf8d42179d6f1
VictorNevesRevature <victor315@revature.net>
2022-12-11 20:25:37 -0500

    majorp1changes

--- ApiProjectOneNew/RepoLayer/Class1.cs
+++ ApiProjectOneNew/RepoLayer/Class1.cs
@@ -1,5 +1,11 @@
 ﻿namespace RepoLayer;
 public class Class1
 {
-
+// Server=tcp:victor-p1server.database.windows.net,1433;
+//Initial Catalog=VictorProjectOne;
+//Persist Security Info=False;
+//User ID=victor203;
+//Password={your_password};
+//MultipleActiveResultSets=False;Encrypt=True;
+//TrustServerCertificate=False;Connection Timeout=30;
 }
---
+++ ApiProjectOneNew/RepoLayer/RepositoryClass.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ModelsLayer;
+
+
+namespace RepoLayer
+{
+    public interface IRepositoryClass
+    {
+        Employee PostEmployee(Employee emp)
+        {
+            //use ADO.NET to push to database
+
+            //call the private get a employee to get an employee
+        }
+        
+    }
+}
